7737fceaeb986470e43c45a53e59f4b2375eeed0
pedro.mendes <pedro.mendes@g.globo>
2022-11-21 13:57:07 -0300

    ux: add styles for header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './assets/css/App.scss';
 import EvaluateSection from './components/evaluateSection';
 import { Layout } from 'antd';
 import logo from './assets/img/logo.svg'
+import playground from './assets/img/playground.svg'
 
 function App() {
   const { Header, Content } = Layout;
 
   return (
     <Layout className="App">
-      <Header style={{ textAlign: 'center' }}>
-        <img src={logo} alt="Risk Based Authentication" style={{width:'300px'}} />
+      <Header className="main_header">
+        <img src={logo} alt="Risk Based Authentication" />
+        <img src={playground} alt="Playground" />
       </Header>
       <Layout>
         <Content className="site-layout" style={{ paddingBottom: '30px', marginTop: '30px' }}>
