6256d84aca749f06fd27f75def65fe67c1fd4475
pedro.mendes <pedro.mendes@g.globo>
2022-11-22 13:49:02 -0300

    ux: finish header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './assets/css/App.scss';
 import EvaluateSection from './components/evaluateSection';
-import { Layout } from 'antd';
+import { Layout, Col,Row } from 'antd';
 import logo from './assets/img/logo.svg'
 import playground from './assets/img/playground.svg'
 
 function App() {
-  const { Header, Content } = Layout;
+  const { Header, Content  } = Layout;
 
   return (
     <Layout className="App">
-      <Header className="main_header">
-        <img src={logo} alt="Risk Based Authentication" />
-        <img src={playground} alt="Playground" />
+      <Header className="main_header" justify="space-between">
+        <Row justify="space-between">
+          <Col span={4}><img src={logo} alt="Risk Based Authentication" /></Col>
+          <Col span={4}><img src={playground} alt="Playground" /></Col>
+        </Row>
       </Header>
       <Layout>
         <Content className="site-layout" style={{ paddingBottom: '30px', marginTop: '30px' }}>
