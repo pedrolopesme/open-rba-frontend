6ed66a08fec003f6abcb06f6a7d902bd0f16be49
pedro.mendes <pedro.mendes@g.globo>
2022-11-23 13:35:33 -0300

    ux: apply styles to evaluation form

--- src/components/evaluateForm.jsx
+++ src/components/evaluateForm.jsx
@@ -1,10 +1,10 @@
 import {React} from 'react';
-import {Form, Input, Button} from 'antd';
+import {Form, Input, Button, Switch, Select} from 'antd';
 import {RightCircleOutlined} from '@ant-design/icons';
 
 const EvaluateForm = ({onFormSubmit}) => {
     const [form] = Form.useForm();
-    const {TextArea} = Input;
+    const { Option } = Select;
 
     const initialContext = `{
         "user_id" : "771cd43f-011d-42fb-9aa6-2b45783e0836",
@@ -21,20 +21,53 @@ const EvaluateForm = ({onFormSubmit}) => {
     return (
     <Form
         form={form}
+        id="evaluationForm"
         onFinish={onFormSubmit}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
         initialValues={{
-            'context':initialContext,
+            'user_id': '88c95a81-0a5b-412a-8f34-72d2dd0600f6'
         }}>
-                <Form.Item 
-                    name={'context'}
-                    rules={[{required: true, message:'This field is required'}]}>
-                        <TextArea placeholder='User Context' rows={12}/>
-                </Form.Item>
+            <Form.Item
+                label="User ID"
+                name="user_id"
+                rules={[{ required: true, message: 'Please provide an user identification.' }]}>
+                 <Input />
+            </Form.Item>
 
-                <Button type="primary" htmlType='submit' block>
-                    Evaluate
-                    <RightCircleOutlined />
-                </Button>
+            <Form.Item
+                label="Authentication Success"
+                name="authentication_success">
+                 <Switch />
+            </Form.Item>
+
+            <Form.Item 
+                name="device_type" 
+                label="Device Type" 
+                rules={[{ required: true, message: 'Please provide a device type.' }]}>
+                <Select
+                    placeholder="Select an option"
+                    allowClear>
+                    <Option value="device1">Desktop - macOS</Option>
+                    <Option value="device2">Desktop - Windows</Option>
+                    <Option value="device3">Desktop - Ubuntu Linux</Option>
+                    <Option value="device4">Mobile - Android</Option>
+                    <Option value="device5">Mobile - IPhone</Option>
+                    <Option value="device6">Tablet - Android</Option>
+                    <Option value="unknwon">Unknown</Option>
+                </Select>
+            </Form.Item>
+
+            {/* <Form.Item 
+                name={'context'}
+                rules={[{required: true, message:'This field is required'}]}>
+                    <TextArea placeholder='User Context' rows={12}/>
+            </Form.Item> */}
+
+            <Button type="primary" htmlType='submit' block>
+                Evaluate
+                <RightCircleOutlined />
+            </Button>
     </Form>);
 }
 
